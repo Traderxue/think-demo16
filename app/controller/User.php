a8f38b69a6cbf4f42fc726499234ad385359aa44
Traderxue <falethe@gmail.com>
2024-01-12 23:07:43 +0800

    init

--- app/controller/User.php
+++ app/controller/User.php
@@ -4,9 +4,56 @@ namespace app\controller;
 use app\BaseController;
 use think\Request;
 use app\model\User as UserModel;
+use app\util\Res;
 
-class User extends BaseController{
-    
+class User extends BaseController
+{
+    private $result;
+
+    function __construct(\think\App $app)
+    {
+        $this->result = new Res();
+    }
+
+    function register(Request $request)
+    {
+        $post = $request->post();
+        $user = new UserModel();
+
+        $u = $user->getByUsername($post["username"]);
+        if ($u) {
+            return $this->result->error("用户已存在");
+        }
+
+        $res = $user->save([
+            "username" => $post["username"],
+            "password" => password_hash($post["password"], PASSWORD_DEFAULT),
+            "add_time" => date("Y-m-d H:i:s"),
+            "ip" => $post->ip()
+        ]);
+
+        if ($res) {
+            return $this->result->success("注册成功", $res);
+        }
+        return $this->result->error("注册失败");
+    }
+
+    function login(Request $request)
+    {
+        $post = $request->post();
+
+        $user = new UserModel();
+
+        $u = $user->getByUsername($post["username"]);
+
+        if (!$u) {
+            return $this->result->error("用户不存在");
+        }
+        if (password_verify($post["password"], $u->password)) {
+            return $this->result->success("登录成功", $u);
+        }
+        return $this->result->error("登录失败");
+    }
 }
 
 
--- app/model/User.php
+++ app/model/User.php
@@ -5,4 +5,8 @@ use think\Model;
 
 class User extends Model{
     protected $table = "user";
+
+    public function getByUsername($username){
+        return $this->where("username",$username)->find();
+    }
 }
